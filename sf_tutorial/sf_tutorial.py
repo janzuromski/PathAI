06ff4187faee6eb030da2fb571a0c070d3226162
j.zuromski@lumc.nl <j.zuromski@lumc.nl>
2025-05-24 21:40:02 +0200

    Restructured tile extraction.

--- sf_tutorial/sf_tutorial.py
+++ sf_tutorial/sf_tutorial.py
@@ -2,41 +2,24 @@ import pandas as pd
 import slideflow as sf
 
 def main():
-    df = pd.read_csv('../annotations_clam_new.csv')
-
-    train_dataset = sf.Dataset(
-        slides='/exports/path-cutane-lymfomen-hpc/Thom_Doeleman/CLAM_train_cropped',
-        tfrecords='project/tfrecords',
-        annotations=df,
-        tile_px=256,
-        tile_um='40x',
-    )
-    # val_dataset = sf.Dataset(
-    #     slides='/exports/path-cutane-lymfomen-hpc/Thom_Doeleman/CLAM_validate_cropped',
-    #     tfrecords='project/tfrecords',
-    #     annotations=df,
-    #     tile_px=256,
-    #     tile_um='10x',
-    # )
     P = sf.create_project(
         root='project',
-        tf
+        annotations='../clam_train_annotations.csv',
+        slides='/exports/path-cutane-lymfomen-hpc/Thom_Doeleman/CLAM_train_cropped'
     )
-    train_dataset.extract_tiles(override_mpp=0.25)
-    # val_dataset.extract_tiles(override_mpp=1)
+    P.extract_tiles(tile_px=256, tile_um='40x', mpp_override=0.25)
 
-    print(f'Extracted {train_dataset.num_tiles} tiles.')
-    labels, _ = train_dataset.labels('category')
+    # labels, _ = train_dataset.labels('category')
 
-    train_dataset = train_dataset.balance('category')
+    # train_dataset = train_dataset.balance('category')
 
-    hp = sf.ModelParams(
-        tile_px=256, tile_um=128, model='xception', batch_size=32, epochs=[3]
-    )
+    # hp = sf.ModelParams(
+    #     tile_px=256, tile_um=128, model='xception', batch_size=32, epochs=[3]
+    # )
 
-    trainer = sf.model.build_trainer(
-        hp=hp, outdir='./project/out/', labels=labels
-    )
+    # trainer = sf.model.build_trainer(
+    #     hp=hp, outdir='./project/out/', labels=labels
+    # )
 
     # results = trainer.train(train_dataset, val_dataset)
 
