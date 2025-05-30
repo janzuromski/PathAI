ffac5ff3077499f99f25ad23707a71fcdb8b5a60
j.zuromski@lumc.nl <j.zuromski@lumc.nl>
2025-05-30 14:34:56 +0200

    Added the validation set to the list of slides to be extracted.

--- clam_mil/tile_extraction.py
+++ clam_mil/tile_extraction.py
@@ -5,19 +5,27 @@ import slideflow as sf
 def main():
     project_dir = 'project'
     extraction_params = (
-        (256, mag) for mag in ('40x', '20x', '10x')
+        (256, mag) for mag in ('10x', '20x', '40x')
     )
     if os.path.exists(project_dir):
         project = sf.load_project(project_dir)
     else:
         project = sf.create_project(
             root=project_dir,
-            annotations='/exports/path-cutane-lymfomen-hpc/jan/rl/clam_train_annotations.csv',
+            annotations='/exports/path-cutane-lymfomen-hpc/jan/rl/annotations_clam_full.csv',
             slides='/exports/path-cutane-lymfomen-hpc/Thom_Doeleman/CLAM_train_cropped'
+        )
+        project.add_source(
+            name='validate',
+            slides='/exports/path-cutane-lymfomen-hpc/Thom_Doeleman/CLAM_validate_cropped'
         )
     for tile_px, tile_um in extraction_params:
         dataset = project.dataset(tile_px=tile_px, tile_um=tile_um)
-        dataset.extract_tiles(mpp_override=0.25, skip_extracted=True)
+        dataset.extract_tiles(
+            mpp_override=0.25,
+            skip_extracted=True,
+            report=False
+        )
     
 
 if __name__ == '__main__':
